ffa7d75930a804a068f4857c7a1a827ac016ab03
tugalsan karabacak <tugalsan@gmail.com>
2022-08-08 13:06:40 +0300

    bug fix

--- src/main/java/com/tugalsan/api/file/html/server/element/TS_FileHtmlEscape.java
+++ src/main/java/com/tugalsan/api/file/html/server/element/TS_FileHtmlEscape.java
@@ -1,11 +1,12 @@
 package com.tugalsan.api.file.html.server.element;
 
 import com.tugalsan.api.compiler.client.*;
+import com.tugalsan.api.file.html.client.*;
 
 public class TS_FileHtmlEscape implements TGS_CompilerType1<String, CharSequence> {
 
     @Override
     public String compile(CharSequence unsafeHtmlText) {
-        return TS_FileHtmlUtils.escape(unsafeHtmlText);
+        return TS_FileHtmlUtils.escape(unsafeHtmlText).replace(" ", TGS_FileHtmlText.charSpace());
     }
 }
